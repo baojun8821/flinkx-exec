eb6fbd7452404e4074d561cf6ec60d2e60b059be
baojun <184097162@qq.com>
2020-12-28 14:17:00 +0800

    localTest

--- flinkx-test/src/main/java/com/dtstack/flinkx/test/LocalTest.java
+++ flinkx-test/src/main/java/com/dtstack/flinkx/test/LocalTest.java
@@ -131,7 +131,7 @@ public class LocalTest {
 //        conf.setString("metrics.reporter.promgateway.randomJobNameSuffix","true");
 //        conf.setString("metrics.reporter.promgateway.deleteOnShutdown","true");
 
-        String jobPath = "D:\\dtstack\\flinkx-all\\flinkx-test\\src\\main\\resources\\dev_test_job\\metadatasqlserver_stream.json";
+        String jobPath = "/Users/baojun/IdeaProjects/yeb-new/flinkx/flinkconf/examples/binlog-mysql.json";
         JobExecutionResult result = LocalTest.runJob(new File(jobPath), confProperties, null);
         ResultPrintUtil.printResult(result);
     }
